8cce9c95ff779fe18a2d52e52aa32b827e28846e
9tun <9tun@daum.net>
2025-10-16 13:52:14 +0900

    Click

---
+++ NRCAD Click/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInterval = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtX = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtY = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtOnOff = New System.Windows.Forms.TextBox()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Interval"
+        '
+        'txtInterval
+        '
+        Me.txtInterval.Location = New System.Drawing.Point(133, 10)
+        Me.txtInterval.Name = "txtInterval"
+        Me.txtInterval.Size = New System.Drawing.Size(119, 21)
+        Me.txtInterval.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 12)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Win Menu X"
+        '
+        'txtX
+        '
+        Me.txtX.Location = New System.Drawing.Point(133, 37)
+        Me.txtX.Name = "txtX"
+        Me.txtX.Size = New System.Drawing.Size(119, 21)
+        Me.txtX.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 12)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Win Menu Y"
+        '
+        'txtY
+        '
+        Me.txtY.Location = New System.Drawing.Point(133, 64)
+        Me.txtY.Name = "txtY"
+        Me.txtY.Size = New System.Drawing.Size(119, 21)
+        Me.txtY.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(38, 94)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 12)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "On/Off"
+        '
+        'txtOnOff
+        '
+        Me.txtOnOff.Location = New System.Drawing.Point(133, 91)
+        Me.txtOnOff.Name = "txtOnOff"
+        Me.txtOnOff.Size = New System.Drawing.Size(119, 21)
+        Me.txtOnOff.TabIndex = 1
+        Me.txtOnOff.Text = "F2"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(41, 130)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(42, 12)
+        Me.lblStatus.TabIndex = 2
+        Me.lblStatus.Text = "Label5"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 161)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.txtOnOff)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtY)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtX)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtInterval)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "NRCAD Click"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtInterval As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtX As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtY As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtOnOff As TextBox
+    Friend WithEvents lblStatus As Label
+End Class
---
+++ NRCAD Click/Form1.vb
@@ -0,0 +1,104 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Threading
+
+Public Class Form1
+    <DllImport("user32.dll")>
+    Private Shared Function RegisterHotKey(hWnd As IntPtr, id As Integer, fsModifiers As Integer, vk As Integer) As Boolean
+    End Function
+    <DllImport("user32.dll")>
+    Private Shared Function UnregisterHotKey(hWnd As IntPtr, id As Integer) As Boolean
+    End Function
+    <DllImport("user32.dll")>
+    Private Shared Function SetCursorPos(ByVal X As Integer, ByVal Y As Integer) As Boolean
+    End Function
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Sub mouse_event(ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As Integer)
+    End Sub
+
+    Private Const HOTKEY_ID As Integer = 1
+    Private Const MOUSEEVENTF_LEFTDOWN As Integer = &H2
+    Private Const MOUSEEVENTF_LEFTUP As Integer = &H4
+    Private keepRunning As Boolean = False
+    Private WithEvents clickTimer As New Windows.Forms.Timer()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim screenWidth As Integer = Screen.PrimaryScreen.Bounds.Width
+        Dim screenHeight As Integer = Screen.PrimaryScreen.Bounds.Height
+
+        ' Only set default if user left field empty
+        If String.IsNullOrWhiteSpace(txtX.Text) Then
+            txtX.Text = (screenWidth * 0.02).ToString("0")   ' 2% from left edge
+        End If
+
+        If String.IsNullOrWhiteSpace(txtY.Text) Then
+            txtY.Text = (screenHeight * 0.97).ToString("0")  ' 97% from top (near bottom)
+        End If
+
+        If String.IsNullOrWhiteSpace(txtInterval.Text) Then
+            txtInterval.Text = "599"   ' Default 599 seconds = nearly 10 mins
+        End If
+
+        If String.IsNullOrWhiteSpace(txtOnOff.Text) Then
+            txtOnOff.Text = "F2"     ' Default hotkey
+        End If
+
+        lblStatus.Text = $"Detected Screen Size: ({screenWidth}x{screenHeight})"
+
+        ' Register the hotkey (using whatever user entered)
+        Dim hotKeyValue As Keys
+        If [Enum].TryParse(txtOnOff.Text, True, hotKeyValue) Then
+            RegisterHotKey(Me.Handle, HOTKEY_ID, 0, hotKeyValue)
+        Else
+            RegisterHotKey(Me.Handle, HOTKEY_ID, 0, Keys.F3)
+        End If
+    End Sub
+
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        Const WM_HOTKEY As Integer = &H312
+        If m.Msg = WM_HOTKEY AndAlso m.WParam.ToInt32() = HOTKEY_ID Then
+            ToggleAutoClick()
+        End If
+        MyBase.WndProc(m)
+    End Sub
+
+    Private Sub ToggleAutoClick()
+        keepRunning = Not keepRunning
+        If keepRunning Then
+            lblStatus.Text = "Running..."
+            MessageBox.Show("Auto click has been turned ON.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            clickTimer.Interval = CInt(Val(txtInterval.Text) * 1000)
+            clickTimer.Start()
+        Else
+            lblStatus.Text = "Stopped"
+            clickTimer.Stop()
+            MessageBox.Show("Auto click has been turned OFF.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub clickTimer_Tick(sender As Object, e As EventArgs) Handles clickTimer.Tick
+        Dim x As Integer = CInt(Val(txtX.Text))
+        Dim y As Integer = CInt(Val(txtY.Text))
+
+        ' Get Cursor Postion
+        SetCursorPos(x, y)
+        Thread.Sleep(100)
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        Thread.Sleep(50)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+
+        ' Show Msg 
+        Dim msg As String = $"Windows Start menu at ({x}, {y})"
+        lblStatus.Text = msg
+        Debug.WriteLine($"[{DateTime.Now:HH:mm:ss}] Clicked at ({x}, {y})")
+
+    End Sub
+
+    Protected Overrides Sub OnFormClosed(e As FormClosedEventArgs)
+        UnregisterHotKey(Me.Handle, HOTKEY_ID)
+        MyBase.OnFormClosed(e)
+    End Sub
+
+    Private Sub txtInterval_TextChanged(sender As Object, e As EventArgs) Handles txtInterval.TextChanged
+
+    End Sub
+End Class
